3b5c08d251000d651c9359b17113621d646983e3
Jason Marshall <jasonmarshallc2@gmail.com>
2021-03-09 07:40:30 -0700

    need to add 2nd sensor

--- rpm/rpm.ino
+++ rpm/rpm.ino
@@ -1,16 +1,23 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-int hall_pin = 2; // digital pin 2 is the hall pin
-float hall_thresh = 10.0; // set number of hall trips for RPM reading (higher improves accuracy)
+
+/*
+ * 
+ * spindle
+ * feed
+*/
+
+int feed_pin = 2; // digital pin 2 is the hall pin
+float feed_thresh = 10.0; // set number of hall trips for RPM reading (higher improves accuracy)
 float start = micros();
-float hall_count = 1;
+float feed_count = 1;
 bool on_state = false;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
-  pinMode(hall_pin, INPUT); // make the hall pin an input:
+  pinMode(feed_pin, INPUT); // make the hall pin an input:
   lcd.begin(16, 2);  // set up the LCD's number of columns and rows:
 
 }
@@ -19,7 +26,7 @@ void displayOut(){
   // print information about Time and RPM
   float end_time = micros();
   float time_passed = ((end_time-start)/1000000.0);
-  float rpm_val = (hall_count/time_passed)*60.0;
+  float rpm_val = (feed_count/time_passed)*60.0;
   Serial.print(rpm_val);
   Serial.println(" RPM");
   delay(1);        // delay in between reads for stability
@@ -39,19 +46,19 @@ void resetDisplay(){
 
 void loop() {
 
-  if (digitalRead(hall_pin)==0){
+  if (digitalRead(feed_pin)==0){
     if (on_state==false){
       on_state = true;
-      hall_count+=1.0;
+      feed_count+=1.0;
     }
   } else{
     on_state = false;
   }
-  if (hall_count>=hall_thresh){
+  if (feed_count>=feed_thresh){
     displayOut();
     start = micros();
     on_state = false;
-    hall_count = 1;
+    feed_count = 1;
   }
 
   if (millis() - (start * 1000) > 1000){
