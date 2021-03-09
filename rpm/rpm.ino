067f17b26ed7d795e8bbe14d7cfee8c671641fce
Jason Marshall <jasonmarshallc2@gmail.com>
2021-03-09 07:43:54 -0700

    orig from Jerry

--- rpm/rpm.ino
+++ rpm/rpm.ino
@@ -1,67 +1,115 @@
+// orig from jerry
+
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-/*
- * 
- * spindle
- * feed
-*/
+// digital pin 2 is the hall pin
 
-int feed_pin = 2; // digital pin 2 is the hall pin
-float feed_thresh = 10.0; // set number of hall trips for RPM reading (higher improves accuracy)
-float start = micros();
-float feed_count = 1;
-bool on_state = false;
+int hall_pin = 2;
+
+// set number of hall trips for RPM reading (higher improves accuracy)
+
+float hall_thresh = 10.0;
+
+ 
 
 void setup() {
+
   // initialize serial communication at 9600 bits per second:
+
   Serial.begin(115200);
-  pinMode(feed_pin, INPUT); // make the hall pin an input:
-  lcd.begin(16, 2);  // set up the LCD's number of columns and rows:
+
+  // make the hall pin an input:
+
+  pinMode(hall_pin, INPUT);
+
+        // set up the LCD's number of columns and rows:
+
+      lcd.begin(16, 2);
 
 }
 
-void displayOut(){
-  // print information about Time and RPM
-  float end_time = micros();
-  float time_passed = ((end_time-start)/1000000.0);
-  float rpm_val = (feed_count/time_passed)*60.0;
-  Serial.print(rpm_val);
-  Serial.println(" RPM");
-  delay(1);        // delay in between reads for stability
+ 
 
-  //Print to lcd
-  lcd.setCursor(0, 0);
-  lcd.println("Hello Jerry");
-  lcd.setCursor(0, 1);
-  lcd.print("FEED ");
-  lcd.print(rpm_val);
-  lcd.println(" RPM");
-}
-
-void resetDisplay(){
-  lcd.clear();
-}
+// the loop routine runs over and over again forever:
 
 void loop() {
 
-  if (digitalRead(feed_pin)==0){
-    if (on_state==false){
-      on_state = true;
-      feed_count+=1.0;
-    }
-  } else{
-    on_state = false;
-  }
-  if (feed_count>=feed_thresh){
-    displayOut();
-    start = micros();
-    on_state = false;
-    feed_count = 1;
-  }
+  // preallocate values for tach
 
-  if (millis() - (start * 1000) > 1000){
-    resetDisplay();
-  }
+  float hall_count = 1.0;
+
+  float start = micros();
+
+  bool on_state = false;
+
+  // counting number of times the hall sensor is tripped
+
+  // but without double counting during the same trip
+
+ 
+
+while(true){
+
+    if (digitalRead(hall_pin)==0){
+
+      if (on_state==false){
+
+        on_state = true;
+
+        hall_count+=1.0;
+
+      }
+
+    } else{
+
+      on_state = false;
+
+    }
+
+    if (hall_count>=hall_thresh){
+
+      break;
+
+    }
+
+} 
+
+  // print information about Time and RPM
+
+  float end_time = micros();
+
+  float time_passed = ((end_time-start)/1000000.0);
+
+  //Serial.print("Time Passed: ");
+
+// Serial.print(time_passed);
+
+  //Serial.println("s");
+
+  float rpm_val = (hall_count/time_passed)*60.0;
+
+  Serial.print(rpm_val);
+
+  Serial.println(" RPM");
+
+  delay(1);        // delay in between reads for stability
+
+ 
+
+  //Print to lcd
+
+      lcd.setCursor(0, 0);
+
+      lcd.print("Hello Jerry");
+
+      lcd.setCursor(0, 1);
+
+      lcd.print("FEED ");
+
+      lcd.print(rpm_val);
+
+      lcd.print(" RPM");
+
 }
