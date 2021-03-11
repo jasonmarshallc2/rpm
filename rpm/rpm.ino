481c84190e5d7e907033aa0817ecf1043749b0e6
Jason Marshall <jasonmarshallc2@gmail.com>
2021-03-11 10:29:51 -0700

    seems to work

--- rpm/rpm.ino
+++ rpm/rpm.ino
@@ -3,67 +3,103 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 int feedPin = 2; // digital pin 2 is the hall pin
 int spindlePin = 3;
 // set number of hall trips for RPM reading (higher improves accuracy)
-float hall_thresh = 5.0;
-unsigned long start;
-float feedCount = 1.0;
-  
+int hall_thresh = 5;
+unsigned long feedStart = micros();
+unsigned long spindleStart = micros();
+//float feedCount = 1.0;
+//feedStart = 0;
+//feedStart = micros();
+bool feedOnState = false;
+bool spindleOnState = false;
+int feedCount = 1;
+int spindleCount = 1;
+float timePassedFeed =0.0;
+float timePassedSpindle =0.0;
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 
   // make the hall pin an input:
   pinMode(feedPin, INPUT);
+  pinMode(spindlePin, INPUT);
         // set up the LCD's number of columns and rows:
       lcd.begin(16, 2);
       lcd.setCursor(0,0);
       lcd.write("Hello");
 }
 
-
-
-// the loop routine runs over and over again forever:
 void loop() {
-  // preallocate values for tach
-  feedCount = 1.0;
 
-  start = 0;
-  start = micros();
-  bool on_state = false;
-  // counting number of times the hall sensor is tripped
-  // but without double counting during the same trip
-  while(true){
-      if (digitalRead(feedPin)==0){
-        if (on_state==false){
-          on_state = true;
-          feedCount+=1.0;
-        }
-      } else{
-        on_state = false;
-      }
-      if (feedCount>=hall_thresh){
-        break;
-      }
-  }
-  updateDisplay(); 
+/*
+ listen for signals
+if counter is met, update the display, reset counter and start times
+*/
+
+ if (digitalRead(feedPin)==LOW){
+   if (feedOnState==false){
+     feedOnState = true;
+     feedCount += 1;
+     }
+   } else {
+     feedOnState = false;
+     }
+   if (feedCount>=hall_thresh){
+     updateDisplay(0); 
+     resetFeedVar();
+     }
+     
+ if (digitalRead(spindlePin)==LOW){
+   if (spindleOnState==false){
+     spindleOnState = true;
+     spindleCount += 1;
+     }
+   } else {
+     spindleOnState = false;
+     }
+   if (spindleCount>=hall_thresh){
+     updateDisplay(1); 
+     resetSpindleVar();
+     }
 }
 
-void updateDisplay(){
-  // print information about Time and RPM
+void resetFeedVar(){
+  feedStart = 0;
+  feedStart = micros();
+  feedOnState = false;
+  feedCount = 1;
+  timePassedFeed = 0.0;
+}
+void resetSpindleVar(){
+  spindleStart = 0;
+  spindleStart = micros();
+  spindleOnState = false;
+  spindleCount = 1;
+  timePassedSpindle = 0.0;
+}
+void updateDisplay(int row){
   unsigned long end_time = micros();
-  float time_passed = ((end_time-start)/1000000.0);
-Serial.print("Time Passed: ");
-Serial.print(time_passed);
-Serial.println("s");
-  float rpm_val = (feedCount/time_passed)*60.0;
-  Serial.print(rpm_val);
+  float rpmVal = 0.0;
+//  lcd.clear();
+  if(row == 0){
+    timePassedFeed = ((end_time-feedStart)/1000000.0);
+    rpmVal = (feedCount/timePassedFeed)*60.0;
+    lcd.setCursor(0, 0);
+    lcd.print("FEED ");
+    lcd.print(rpmVal);
+    lcd.print(" RPM");
+  } else {
+    timePassedSpindle = ((end_time-spindleStart)/1000000.0);
+    rpmVal = (spindleCount/timePassedSpindle)*60.0;
+    lcd.setCursor(0, 1);
+    lcd.print("SPINDLE ");
+    lcd.print(rpmVal);
+    lcd.print(" RPM");
+  }
+
+
+  Serial.print(rpmVal);
   Serial.println(" RPM");
   delay(1);        // delay in between reads for stability
-  //Print to lcd
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Hello Jerry");
-      lcd.setCursor(0, 1);
-      lcd.print("FEED ");
-      lcd.print(rpm_val);
-      lcd.print(" RPM");
+
 }
